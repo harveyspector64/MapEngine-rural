c9de16c31466ae3b062a91421e1c3c6d2633357a
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-07 15:37:31 -0700

    Update TileSetInit.js

--- src/tiles/TileSetInit.js
+++ src/tiles/TileSetInit.js
@@ -10,36 +10,36 @@ import TileSet from './TileSet.js';
 export function initializeTileSet() {
     const tileSet = new TileSet();
 
-    tileSet.tiles['grass'] = new Tile('grass', 'assets/grass.png');
-    tileSet.tiles['field'] = new Tile('field', 'assets/dirt.png');
-    tileSet.tiles['dirt'] = new Tile('dirt', 'assets/dirt.png');
-    tileSet.tiles['road'] = new Tile('road', 'assets/road.png');
-    tileSet.tiles['tree'] = new Tile('tree', 'assets/tree.png');
-    tileSet.tiles['bush'] = new Tile('bush', 'assets/bush.png');
-    tileSet.tiles['hill'] = new Tile('hill', 'assets/hill.png');
-    tileSet.tiles['barn'] = new Tile('barn', 'assets/barn.png');
-    tileSet.tiles['silo'] = new Tile('silo', 'assets/silo.png');
+    tileSet.addTile(new Tile('grass', 'assets/grass.png'));
+    tileSet.addTile(new Tile('field', 'assets/dirt.png'));
+    tileSet.addTile(new Tile('dirt', 'assets/dirt.png'));
+    tileSet.addTile(new Tile('road', 'assets/road.png'));
+    tileSet.addTile(new Tile('tree', 'assets/tree.png'));
+    tileSet.addTile(new Tile('bush', 'assets/bush.png'));
+    tileSet.addTile(new Tile('hill', 'assets/hill.png'));
+    tileSet.addTile(new Tile('barn', 'assets/barn.png'));
+    tileSet.addTile(new Tile('silo', 'assets/silo.png'));
 
-    tileSet.tiles['grass'].addConnection('grass');
-    tileSet.tiles['grass'].addConnection('field');
-    tileSet.tiles['grass'].addConnection('dirt');
-    tileSet.tiles['grass'].addConnection('tree');
-    tileSet.tiles['grass'].addConnection('bush');
-    tileSet.tiles['grass'].addConnection('hill');
+    tileSet.getTile('grass').addConnection('grass');
+    tileSet.getTile('grass').addConnection('field');
+    tileSet.getTile('grass').addConnection('dirt');
+    tileSet.getTile('grass').addConnection('tree');
+    tileSet.getTile('grass').addConnection('bush');
+    tileSet.getTile('grass').addConnection('hill');
 
-    tileSet.tiles['field'].addConnection('field');
-    tileSet.tiles['field'].addConnection('grass');
+    tileSet.getTile('field').addConnection('field');
+    tileSet.getTile('field').addConnection('grass');
 
-    tileSet.tiles['dirt'].addConnection('dirt');
-    tileSet.tiles['dirt'].addConnection('road');
-    tileSet.tiles['dirt'].addConnection('grass');
+    tileSet.getTile('dirt').addConnection('dirt');
+    tileSet.getTile('dirt').addConnection('road');
+    tileSet.getTile('dirt').addConnection('grass');
 
-    tileSet.tiles['road'].addConnection('road');
-    tileSet.tiles['road'].addConnection('dirt');
+    tileSet.getTile('road').addConnection('road');
+    tileSet.getTile('road').addConnection('dirt');
 
-    tileSet.tiles['tree'].addConnection('grass');
-    tileSet.tiles['bush'].addConnection('grass');
-    tileSet.tiles['hill'].addConnection('grass');
+    tileSet.getTile('tree').addConnection('grass');
+    tileSet.getTile('bush').addConnection('grass');
+    tileSet.getTile('hill').addConnection('grass');
 
     console.log('TileSet initialized:', tileSet);
     return tileSet;
