b7753350ac7edf408853ecc0f70489a8f82ca51f
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-07 15:22:39 -0700

    Update Tile.js

--- src/tiles/Tile.js
+++ src/tiles/Tile.js
@@ -1,26 +1,26 @@
 // src/tiles/Tile.js
 
 /**
- * Class representing a tile in the map.
+ * Class representing a single tile.
  */
 class Tile {
     /**
      * Create a tile.
      * @param {string} type - The type of the tile.
-     * @param {string} image - The path to the tile's image.
+     * @param {string} image - The image source of the tile.
      */
     constructor(type, image) {
         this.type = type;
         this.image = image;
-        this.connectsTo = []; // Array of tile types this tile can connect to
+        this.connections = new Set();
     }
 
     /**
      * Add a connection to another tile type.
-     * @param {string} type - The type of the tile to connect to.
+     * @param {string} connectionType - The type of the tile to connect to.
      */
-    addConnection(type) {
-        this.connectsTo.push(type);
+    addConnection(connectionType) {
+        this.connections.add(connectionType);
     }
 }
 
