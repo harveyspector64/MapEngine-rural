49132727f182bdd529f0f6c447c83b3d242fbf56
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-07 17:35:28 -0700

    Update TileSet.js

--- src/tiles/TileSet.js
+++ src/tiles/TileSet.js
@@ -16,7 +16,7 @@ class TileSet {
      */
     addTile(tile) {
         this.tiles[tile.type] = tile;
-        console.log(`Tile added: ${tile.type} with image ${tile.image}`);  // Debugging
+        console.log(`Tile added: ${tile.type} with image ${tile.image}`);  // Detailed logging
     }
 
     /**
@@ -27,6 +27,8 @@ class TileSet {
     getTile(type) {
         if (!this.tiles[type]) {
             console.error(`Tile not found during getTile: ${type}`);
+        } else {
+            console.log(`Tile retrieved: ${type} with image ${this.tiles[type].image}`);  // Detailed logging
         }
         return this.tiles[type];
     }
